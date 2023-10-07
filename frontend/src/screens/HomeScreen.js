a1971b0df89e621fe8ad5ccb78c85c1fe35e4ee7
Diego Cosacov <d.cosacow@gmail.com>
2023-10-07 23:36:39 +0300

    Add react-boostrap and divide to head and footer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 //import data from '../data';
 import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -18,7 +17,7 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
